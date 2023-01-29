12e67c43842df6ca7e0c1d6b30427fc6830b6a34
VERO MENICHETTI <ingvmenichetti@gmail.com>
2023-01-29 17:47:20 -0300

    Se creo error

--- proy3-frontend/src/App.js
+++ proy3-frontend/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Error from './components/views/Error';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
     return (
         <div>
+           <Error></Error>
         </div>
     );
 };
