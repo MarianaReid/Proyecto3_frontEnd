19f0c99b310428f8484e2fe26b30abd4e6fe6b98
Andressa Arcocha <andressaarcocha@hotmail.com>
2023-01-29 11:19:36 -0300

    se creo la logica para mandar email al registrarse

---
+++ proy3-frontend/src/components/views/mandarMail.js
@@ -0,0 +1,20 @@
+import emailjs from '@emailjs/browser';
+import React from 'react';
+
+export const sendMail = (nombre, email)=>{
+    const EMAILJS_SERVICE = process.env.REACT_APP_EMAILJS_SERVICE;
+    const EMAILJS_TEMPLATE = process.env.REACT_APP_EMAILJS_TEMPLATE;
+    const EMAILJS_PUBLICKEY = process.env.REACT_APP_EMAILJS_PUBLICKEY;
+
+    var templateParams = {
+        user_name: nombre,
+        message: 'Se pudo registrar exitosamente, muchas gracias.',
+        destinatario: email,
+        reply_to: 'saboreslatinos46@gmail.com'
+    };
+    emailjs.send(EMAILJS_SERVICE, EMAILJS_TEMPLATE, templateParams, EMAILJS_PUBLICKEY)
+        .then(function(response) {
+        }, function(error) {
+        });
+}
+
