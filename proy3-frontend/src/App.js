5295b2de57bcb4696b02c15fb2d6fbc3da81b1c7
Emmanuel Rizza <emanuelrizza@gmail.com>
2023-01-30 20:12:25 -0300

    actualizacion en navbar

--- proy3-frontend/src/App.js
+++ proy3-frontend/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Registro from './components/views/Registro';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import NavScroll from './components/views/Navbar';
 
 const App = () => {
     return (
         <div>
+            <NavScroll></NavScroll>
            <Registro></Registro>
         </div>
     );
--- proy3-frontend/src/components/views/Navbar.jsx
+++ proy3-frontend/src/components/views/Navbar.jsx
@@ -1,8 +1,27 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
-const Navbar = () => {
+const NavScroll = () => {
   return (
-    <div>Aqui va Navbar</div>
-  )
+    <Navbar bg="dark" variant="dark" expand="lg" fixed="top">
+      <Container fluid>
+        <Navbar.Brand href="#">Sabores Latinos</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: '100px' }}
+            navbarScroll>            
+            <Nav.Link href="#action2">Login</Nav.Link>            
+            <Nav.Link href="#">
+              Registro
+            </Nav.Link>
+          </Nav>          
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
 
-export default Navbar
+export default NavScroll;
