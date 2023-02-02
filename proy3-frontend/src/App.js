35eee347f0d11164038286c2fdbd4b7c4d6ce3c5
SteveO <orregoe@gmail.com>
2023-02-01 21:59:55 -0300

    Cambio de navbar

--- proy3-frontend/src/App.js
+++ proy3-frontend/src/App.js
@@ -1,14 +1,11 @@
 import React from 'react';
-import Registro from './components/views/Registro';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import NavScroll from './components/views/Navbar';
 
 const App = () => {
     return (
-        <div>
-            <NavScroll></NavScroll>
-           <Registro></Registro>
-        </div>
+        <>
+
+        </>
     );
 };
 
--- proy3-frontend/src/components/views/Navbar.jsx
+++ proy3-frontend/src/components/views/Navbar.jsx
@@ -1,27 +1,36 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import Offcanvas from 'react-bootstrap/Offcanvas';
 
-const NavScroll = () => {
+const OffCanvas = () => {
   return (
-    <Navbar bg="dark" variant="dark" expand="lg" fixed="top">
-      <Container fluid>
-        <Navbar.Brand href="#">Sabores Latinos</Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
-          <Nav
-            className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: '100px' }}
-            navbarScroll>            
-            <Nav.Link href="#action2">Login</Nav.Link>            
-            <Nav.Link href="#">
-              Registro
-            </Nav.Link>
-          </Nav>          
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <>
+        <Navbar bg="light" expand={false} className="mb-3">
+          <Container fluid>
+            <Navbar.Brand href="#">Sabores Latinos</Navbar.Brand>
+            <Navbar.Toggle aria-controls="offcanvasNavbar-expand-false" />
+            <Navbar.Offcanvas
+              id="offcanvasNavbar-expand-false"
+              aria-labelledby="offcanvasNavbarLabel-expand-false"
+              placement="end"
+            >
+              <Offcanvas.Header closeButton>
+                <Offcanvas.Title id="offcanvasNavbarLabel-expand-false">
+                  Menu
+                </Offcanvas.Title>
+              </Offcanvas.Header>
+              <Offcanvas.Body>
+                <Nav className="justify-content-end flex-grow-1 pe-3">
+                  <Nav.Link href="#action1">Login</Nav.Link>
+                  <Nav.Link href="#action2">Registrar</Nav.Link>
+                </Nav>
+              </Offcanvas.Body>
+            </Navbar.Offcanvas>
+          </Container>
+        </Navbar>
+    </>
   );
 }
 
-export default NavScroll;
+export default OffCanvas;
