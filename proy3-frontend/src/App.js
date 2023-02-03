a17907d96c44c04d46b3643cb326fb9f5e7c404c
Emmanuel Rizza <emanuelrizza@gmail.com>
2023-02-03 01:34:34 -0300

    arreglos en footer

--- proy3-frontend/src/App.js
+++ proy3-frontend/src/App.js
@@ -5,10 +5,12 @@ import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 
 
 import "@fortawesome/fontawesome-free/css/all.min.css";
+import Footer from './components/Footer';
 
 const App = () => {
     return (
-        <>                     
+        <>
+        <Footer></Footer>                     
         </>
     );
 };
--- proy3-frontend/src/components/Footer.jsx
+++ proy3-frontend/src/components/Footer.jsx
@@ -3,34 +3,24 @@ import { MDBFooter, MDBContainer, MDBRow, MDBCol, MDBIcon } from 'mdb-react-ui-k
 
 const Footer = () => {
   return (
-    <MDBFooter bgColor='light' className='text-center text-lg-start text-muted'>
+    <MDBFooter bgColor='dark' className='text-center text-lg-start text-white'>
       <section className='d-flex justify-content-center justify-content-lg-between p-4 border-bottom'>
         <div className='me-5 d-none d-lg-block'>
           <span>Get connected with us on social networks:</span>
         </div>
 
         <div>
-          <a href='#/' className='me-4 text-reset'>
+          <a href='https://es-la.facebook.com/' target={'_blank'} className='me-4 text-reset' rel="noreferrer">
             <MDBIcon fab icon="facebook-f" />
           </a>
-          <a href='#/' className='me-4 text-reset'>
+          <a href='https://twitter.com/?lang=es' target={'_blank'} className='me-4 text-reset' rel="noreferrer">
             <MDBIcon fab icon="twitter" />
           </a>
-          <a href='#/' className='me-4 text-reset'>
-            <MDBIcon fab icon="google" />
-          </a>
-          <a href='#/' className='me-4 text-reset'>
+          <a href='https://www.instagram.com/' target={'_blank'} className='me-4 text-reset' rel="noreferrer">
             <MDBIcon fab icon="instagram" />
-          </a>
-          <a href='#/' className='me-4 text-reset'>
-            <MDBIcon fab icon="linkedin" />
-          </a>
-          <a href='#/' className='me-4 text-reset'>
-            <MDBIcon fab icon="github" />
           </a>
         </div>
       </section>
-
       <section className=''>
         <MDBContainer className='text-center text-md-start mt-5'>
           <MDBRow className='mt-3'>
@@ -44,70 +34,37 @@ const Footer = () => {
                 consectetur adipisicing elit.
               </p>
             </MDBCol>
-
-            <MDBCol md="2" lg="2" xl="2" className='mx-auto mb-4'>
-              <h6 className='text-uppercase fw-bold mb-4'>Products</h6>
-              <p>
-                <a href='#!' className='text-reset'>
-                  Angular
-                </a>
-              </p>
-              <p>
-                <a href='#!' className='text-reset'>
-                  React
-                </a>
-              </p>
-              <p>
-                <a href='#!' className='text-reset'>
-                  Vue
-                </a>
-              </p>
-              <p>
-                <a href='#!' className='text-reset'>
-                  Laravel
-                </a>
-              </p>
-            </MDBCol>
-
             <MDBCol md="3" lg="2" xl="2" className='mx-auto mb-4'>
-              <h6 className='text-uppercase fw-bold mb-4'>Useful links</h6>
+              <h6 className='text-uppercase fw-bold mb-4'>INGRESA</h6>
               <p>
                 <a href='#!' className='text-reset'>
-                  Pricing
+                  Home
                 </a>
               </p>
               <p>
                 <a href='#!' className='text-reset'>
-                  Settings
+                  Login
                 </a>
               </p>
               <p>
                 <a href='#!' className='text-reset'>
-                  Orders
-                </a>
-              </p>
-              <p>
-                <a href='#!' className='text-reset'>
-                  Help
+                  Registrate
                 </a>
               </p>
             </MDBCol>
 
             <MDBCol md="4" lg="3" xl="3" className='mx-auto mb-md-0 mb-4'>
-              <h6 className='text-uppercase fw-bold mb-4'>Contact</h6>
+              <h6 className='text-uppercase fw-bold mb-4'>Contacto</h6>
               <p>
                 <MDBIcon icon="home" className="me-2" />
-                New York, NY 10012, US
+                Gral. Paz 700
               </p>
               <p>
                 <MDBIcon icon="envelope" className="me-3" />
-                info@example.com
+                saboreslatinos@gmail.com
               </p>
               <p>
-                <MDBIcon icon="phone" className="me-3" /> + 01 234 567 88
-              </p>
-              <p>
-                <MDBIcon icon="print" className="me-3" /> + 01 234 567 89
+                <MDBIcon icon="phone" className="me-3" /> +54 9 3815655412
               </p>
             </MDBCol>
           </MDBRow>
@@ -115,9 +72,9 @@ const Footer = () => {
       </section>
 
       <div className='text-center p-4' style={{ backgroundColor: 'rgba(0, 0, 0, 0.05)' }}>
-        © 2021 Copyright:
-        <a className='text-reset fw-bold' href='https://mdbootstrap.com/'>
-          MDBootstrap.com
+        © 2023 Copyright:
+        <a className='text-reset fw-bold' href='#/'>
+          saboreslatinos.com
         </a>
       </div>
     </MDBFooter>
