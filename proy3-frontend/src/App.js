1f46416a0d4400edfa18051796c1918f8085a2b6
SteveO <orregoe@gmail.com>
2023-02-03 01:05:32 -0300

    fix footer

--- proy3-frontend/src/App.js
+++ proy3-frontend/src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Footer from './components/Footer';
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+
+
+
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
 const App = () => {
     return (
-        <div>                     
-            <Footer></Footer>
-        </div>
+        <>                     
+        </>
     );
 };
 
