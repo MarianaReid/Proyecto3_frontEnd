acf8693c5e8b94c6c8832e9e3e2825951c97125c
SteveO <orregoe@gmail.com>
2023-02-04 18:40:49 -0300

    integracion error con rama dev

--- proy3-frontend/src/pages/Error.jsx
+++ proy3-frontend/src/pages/Error.jsx
@@ -1,13 +1,22 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import error from './img/error.jpg'
+import { Link } from 'react-router-dom';
+import "./Error.css";
 
 const Error = () => {
     return (
-        <div>
-            <h3>404</h3>
-            <Link to="/">Volver Home</Link>
-        </div>
-    )
-}
+        <section className="fondo text-center">
+      <div className="container pt-4">
+        <img src={error} className='logo' alt='imagen de error'></img>
+        <h3 className="pt-5 letra">Ooops...! No se encontro lo que estas buscando</h3>
+        <Link to='/' className='ov-btn-slide-bottom'>VOLVER A INICIO</Link>
+      </div>
+      {/* <div className="container-notfound container">
+        <h3 className="notfound pt-5">No se encontro lo que estas buscando</h3>
+        <Link to="/" className="btn btn-dark bg-black my-3 botonVolver">{props.usuarioLogueado.perfil ? "Volver a Inicio" : "Volver a Comprar"}</Link>
+      </div> */}
+    </section>
+  );
+};
 
-export default Error
+export default Error;
