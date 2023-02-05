bd494f84b7a02d50f452a1f0ae3a52dcfc67a48d
SteveO <orregoe@gmail.com>
2023-02-05 17:48:43 -0300

    Integracion login front con api

--- proy3-frontend/src/components/Navbar.jsx
+++ proy3-frontend/src/components/Navbar.jsx
@@ -1,10 +1,20 @@
+import { useEffect, useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Offcanvas from 'react-bootstrap/Offcanvas';
 import { NavLink } from 'react-router-dom';
+import { getLocalStorage } from '../utils/LocalStorageHelper';
 
 const OffCanvas = () => {
+  
+  //Ver lo de consultar el usuario logeado
+  const [isLogged, setIsLogged] = useState({});
+
+  useEffect(() => {
+    setIsLogged(getLocalStorage("userLogged"));
+  }, []);
+
   return (
     <>
       <Navbar bg="light" expand={false} className="mb-3">
@@ -25,12 +35,24 @@ const OffCanvas = () => {
             </Offcanvas.Header>
             <Offcanvas.Body>
               <Nav className="justify-content-end flex-grow-1 pe-3">
-                <Nav.Link>
-                <NavLink to="/login">Login</NavLink>
-                </Nav.Link>
-                <Nav.Link>
-                <NavLink to="/register">Registrar</NavLink>
-                </Nav.Link>
+                {!isLogged ? (
+                  <>
+                    <Nav.Link>
+                      <NavLink to="/login">Login</NavLink>
+                    </Nav.Link><Nav.Link>
+                      <NavLink to="/register">Registrar</NavLink>
+                    </Nav.Link>
+                  </>
+                ) : (
+                  <>
+                    <Nav.Link>
+                      <div>{isLogged.name}</div>
+                    </Nav.Link><Nav.Link>
+                      <NavLink to="/">Cerrar sesion</NavLink>
+                    </Nav.Link>
+                  </>
+                )
+                }
               </Nav>
             </Offcanvas.Body>
           </Navbar.Offcanvas>
