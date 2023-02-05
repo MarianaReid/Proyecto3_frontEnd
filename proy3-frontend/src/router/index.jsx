0cfbbc7d928f7588a2a09b77e4aec73831862935
SteveO <orregoe@gmail.com>
2023-02-04 21:28:48 -0300

    Add page search products

--- proy3-frontend/src/router/index.jsx
+++ proy3-frontend/src/router/index.jsx
@@ -3,6 +3,7 @@ import LayoutPublic from "../layout/LayoutPublic";
 import Error from "../pages/Error";
 import Home from "../pages/Home";
 import Login from "../pages/Login";
+import Products from "../pages/Products";
 import Registro from "../pages/Registro";
 
 export const router = createBrowserRouter([
@@ -22,6 +23,10 @@ export const router = createBrowserRouter([
             {
                 path: "/register",
                 element: <Registro />,
+            },
+            {
+                path: "/products",
+                element: <Products />,
             },
             {
                 path: "/error",
