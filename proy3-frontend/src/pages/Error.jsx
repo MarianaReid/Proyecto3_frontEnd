d0e5c9379571227a0e6ba9251b4ab9bcc0669b48
SteveO <orregoe@gmail.com>
2023-02-04 21:23:22 -0300

    fix image page error

--- proy3-frontend/src/pages/Error.jsx
+++ proy3-frontend/src/pages/Error.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
-import error from './img/error.jpg'
 import { Link } from 'react-router-dom';
 import "./Error.css";
+import { Image } from 'react-bootstrap';
 
 const Error = () => {
     return (
         <section className="fondo text-center">
       <div className="container pt-4">
-        <img src={error} className='logo' alt='imagen de error'></img>
+        {/* <img src={error} className='logo' alt='imagen de error'></img> */}
+        <Image src='https://i.postimg.cc/76cT4ZbD/error.jpg' fluid className='logo'/>
         <h3 className="pt-5 letra">Ooops...! No se encontro lo que estas buscando</h3>
         <Link to='/' className='ov-btn-slide-bottom'>VOLVER A INICIO</Link>
       </div>
