80f11f307669c15657db320641c9816ab99e7307
SteveO <orregoe@gmail.com>
2023-02-21 23:00:19 -0300

    cambio funte de imagen 404

--- proy3-frontend/src/pages/Error.jsx
+++ proy3-frontend/src/pages/Error.jsx
@@ -8,7 +8,7 @@ const Error = () => {
         <section className="fondo text-center">
       <div className="container pt-4">
         {/* <img src={error} className='logo' alt='imagen de error'></img> */}
-        <Image src='https://i.postimg.cc/76cT4ZbD/error.jpg' fluid className='logo'/>
+        <Image src='https://i.postimg.cc/TPVDbZ1h/404.jpg' fluid className='logo'/>
         <h3 className="pt-5 letra">Ooops...! No se encontro lo que estas buscando</h3>
         <Link to='/' className='ov-btn-slide-bottom'>VOLVER A INICIO</Link>
       </div>
