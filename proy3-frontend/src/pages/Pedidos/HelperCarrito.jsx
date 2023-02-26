f51dd35eb11d7d78103b4d96b9ac57ae20138fe5
Andressa Arcocha <andressaarcocha@hotmail.com>
2023-02-26 17:09:20 -0300

    se creo la funcion de agregarcarrito

--- proy3-frontend/src/pages/Pedidos/HelperCarrito.jsx
+++ proy3-frontend/src/pages/Pedidos/HelperCarrito.jsx
@@ -1,3 +1,39 @@
+import Swal from "sweetalert2";
+
+export const AgregarCarrito = (producto) => {
+    // const usuarioLogueado = "https://proyecto3-rolling-code.vercel.app/api/login"  null;
+
+
+    if (usuarioLogueado != null) {
+        // let productosPedido = JSON.parse(localStorage.getItem("https://proyecto3-rolling-code-los-crack.vercel.app/api/products%22))  [];
+
+
+        let i = productosPedido.findIndex((item)=>{return item._id === producto._id});
+
+        if (i>=0) {
+          if (productosPedido[i].cantidad !== undefined)
+            productosPedido[i].cantidad++;
+          else
+            productosPedido[i].cantidad=1;
+        }
+        else
+        {
+          producto.cantidad = 1;
+          productosPedido.push(producto);
+        }
+
+        // localStorage.setItem("https://proyecto3-rolling-code-los-crack.vercel.app/api/products", JSON.stringify(productosPedido));
+        Swal.fire(
+          'Producto agregado',
+          'El producto fue agregado correctamente',
+          'success'
+        );
+
+        return true;
+    }
+    return false;
+}
+
 export const formatMoneda=(price)=>{
     return `$${price}`;
 }
